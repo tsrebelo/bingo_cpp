18b87a898ebefc611b981c53013bf848dddc722e
Telma Rebelo <rebelotelma01@gmail.com>
2024-01-10 12:32:29 -0100

    bingo.cpp

--- bingo.cpp
+++ bingo.cpp
@@ -9,13 +9,32 @@ int main(){
     srand((unsigned) time(0));
 
     int sorteio;
-    cout << "Escolhe o teu sorteio (75, 90 ou 100): " << endl;
+    cout << "Escolhe a quantidade de números para o teu sorteio (75, 90 ou 100): ";
     cin >> sorteio;
 
 
+    int numerosMax;
+
     switch (sorteio){
+
         case 75:
+        numerosMax = 75;
+        break;
+
+        case 90:
+        numerosMax = 90;
+        break;
+
+        case 100:
+        numerosMax = 100;
+        break;
+
+      default:
+        cout << "Número de sorteio inválido." << endl;
+        break;
     }
 
-
+    
+    cout << "Sorteio de " << numerosMax << " números" << endl;
+    return 0;
 }
